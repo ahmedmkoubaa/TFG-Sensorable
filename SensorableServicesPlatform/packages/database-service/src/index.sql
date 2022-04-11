42737d2a0c07d58046bcc072111e39976b93e2a2
ahmedmkoubaa <ahmedmkoubaa@correo.ugr.es>
2022-04-11 17:19:34 +0200

    fix: added ";" to sql queries refs: #TFG-88 #TFG-87

--- SensorableServicesPlatform/packages/database-service/src/index.sql
+++ SensorableServicesPlatform/packages/database-service/src/index.sql
@@ -10,14 +10,13 @@ CREATE TABLE sensors(
 
 /*EXAMPLE OF INSERT:
  INSERT INTO sensors (device_type, sensor_type, values_x, values_y, values_z, timestamp) VALUES (0,21, 67, -1, -1, 1234567981)
- */
-
+ */;
 
 CREATE TABLE adls (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(500) NOT NULL
-) 
+);
 
 CREATE TABLE events (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -26,7 +25,7 @@ CREATE TABLE events (
     pos INT DEFAULT 0,
     operator ENUM('GREATER', 'LESS', 'EQUAL', 'GREATER_EQUAL', 'LESS_EQUAL', 'NOT_EQUAL') NOT NULL,
     operand FLOAT NOT NULL
-)
+);
 
 CREATE TABLE events_for_adls (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -35,4 +34,4 @@ CREATE TABLE events_for_adls (
 
     FOREIGN KEY (id_adl) REFERENCES adls(id) ON DELETE CASCADE,
     FOREIGN KEY (id_event) REFERENCES events(id) ON DELETE CASCADE
-)
+);

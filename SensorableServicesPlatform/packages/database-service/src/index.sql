e3228c6cd3f831240fdb225d046af67d4b560e9f
ahmedmkoubaa <ahmedmkoubaa@correo.ugr.es>
2022-05-27 20:58:55 +0200

    feat: added new field to mysql database
    The adl's structure has to be completedly adapted, for now we
    have to continue adapting since the adl number 5.
    
    refs: #TFG-95

--- SensorableServicesPlatform/packages/database-service/src/index.sql
+++ SensorableServicesPlatform/packages/database-service/src/index.sql
@@ -5,6 +5,32 @@ events_for_adls,
 events,
 adls;
 
+DECLARE DEVICE_MOBILE CONSTANT number (1) := 0;
+
+DECLARE DEVICE_WEAR_OS CONSTANT number (1) := 1;
+
+DECLARE DEVICE_EMPATICA CONSTANT number (1) := 2;
+
+DECLARE TYPE_ACCELEROMETER CONSTANT number (1) := 1;
+
+DECLARE TYPE_LIGHT CONSTANT number (1) := 5;
+
+DECLARE TYPE_PRESSURE CONSTANT number (1) := 6;
+
+DECLARE TYPE_LINEAR_ACCELERATION CONSTANT number (1) := 10;
+
+DECLARE TYPE_RELATIVE_HUMIDITY CONSTANT number (1) := 12;
+
+DECLARE TYPE_AMBIENT_TEMPERATURE CONSTANT number (1) := 13;
+
+DECLARE TYPE_STEP_DETECTOR CONSTANT number (1) := 18;
+
+DECLARE TYPE_STEP_COUNTER CONSTANT number (1) := 19;
+
+DECLARE TYPE_HEART_RATE CONSTANT number (1) := 21;
+
+DECLARE TYPE_HEART_RATE CONSTANT number (1) := 2411;
+
 /*EXAMPLE OF INSERT:
  INSERT INTO sensors (device_type, sensor_type, values_x, values_y, values_z, timestamp) VALUES (0,21, 67, -1, -1, 1234567981)*/
 CREATE TABLE sensors(
@@ -55,57 +81,20 @@ CREATE TABLE events_for_adls (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_adl INT,
     id_event INT,
+    version INT DEFAULT 1,
     FOREIGN KEY (id_adl) REFERENCES adls(id) ON DELETE CASCADE,
     FOREIGN KEY (id_event) REFERENCES events(id) ON DELETE CASCADE,
-    UNIQUE KEY(id_adl, id_event)
+    UNIQUE KEY(id_adl, id_event, version)
 );
 
+/*----------------------------------------------------------------------*/
+/*phone call adl detection*/
 INSERT INTO
     adls(title, description)
 VALUES
     (
         "Llamada telefónica detectada",
-        "En base a criterios del sistema juzgando la postura del usuario se ha detectado una posible llamada teléfonica."
-    );
-
-INSERT INTO
-    adls(title, description)
-VALUES
-    (
-        "Paseo por la calle, en exteriores",
-        "Se ha detectado una caminata a ritmo ligero fuera de casa."
-    );
-
-INSERT INTO
-    adls(title, description)
-VALUES
-    (
-        "Paseo por casa, en interior",
-        "Se ha detectado al usuario andando dentro de casa."
-    );
-
-INSERT INTO
-    adls(title, description)
-VALUES
-    (
-        "Actividad física avanzada",
-        "Se ha detectado una actividad física detectada, el ritmo cardíaco subió durante la actividad y hubo bastante movimiento."
-    );
-
-INSERT INTO
-    adls(title, description)
-VALUES
-    (
-        "Visita a casa de amigos detectada",
-        "El usuario ha estado cerca de la casa de unos amigos."
-    );
-
-INSERT INTO
-    adls(title, description)
-VALUES
-    (
-        "Desplazamiento por cuenta propia usando un medio de transporte",
-        "Aparentemente se ha usado un medio de transporte para desplazarse."
+        "En base a criterios del sistema, juzgando la postura del usuario y demás, se ha detectado una posible llamada teléfonica."
     );
 
 /*events for phone call*/
@@ -129,32 +118,66 @@ INSERT INTO
 VALUES
     (0, 1, 0, 'LESS_EQUAL', -4);
 
+/*events for adls to detect a phone call*/
+/*version 1 (left hand)*/
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (1, 1, 1);
+
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (1, 2, 1);
+
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (1, 3, 1);
+
+/*version 2 (right hand)*/
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (1, 1, 2);
+
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (1, 2, 2);
+
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (1, 4, 2);
+
 /*----------------------------------------------------------------------*/
+INSERT INTO
+    adls(title, description)
+VALUES
+    (
+        "Paseo por la calle, en exteriores",
+        "Se ha detectado una caminata a ritmo ligero fuera de casa."
+    );
+
 /*events to detect a walk*/
 /*5*/
 INSERT INTO
-    events(
-        id,
-        device_type,
-        sensor_type,
-        pos,
-        operator,
-        operand
-    )
+    events(device_type, sensor_type, pos, operator, operand)
 VALUES
-    (5, 0, 19, 0, 'GREATER_EQUAL', 1);
+    (0, 10, 4, 'GREATER', 1.5);
 
 /*6*/
 INSERT INTO
     events(device_type, sensor_type, pos, operator, operand)
 VALUES
-    (0, 10, 4, 'GREATER', 1);
+    (0, 19, 0, 'GREATER_EQUAL', 1);
 
 /*7*/
 INSERT INTO
     events(device_type, sensor_type, pos, operator, operand)
 VALUES
-    (0, 21, 0, 'GREATER', 50);
+    (0, 21, 0, 'GREATER', 60);
 
 /*8*/
 INSERT INTO
@@ -162,8 +185,8 @@ INSERT INTO
 VALUES
     (0, 21, 0, 'LESS', 80);
 
-/*far from home*/
 /*9*/
+/*far from home*/
 INSERT INTO
     events(
         device_type,
@@ -174,101 +197,7 @@ INSERT INTO
         tag
     )
 VALUES
-    (0, 2411, 3, 'GREATER', 100, "HOME");
-
-/*10*/
-/*close from home*/
-INSERT INTO
-    events(
-        device_type,
-        sensor_type,
-        pos,
-        operator,
-        operand,
-        tag
-    )
-VALUES
-    (0, 2411, 3, 'LESS_EQUAL', 100, "HOME");
-
-/*events for workout detection (the previous were also for this)*/
-/*11*/
-INSERT INTO
-    events(device_type, sensor_type, pos, operator, operand)
-VALUES
-    (0, 21, 0, 'GREATER_EQUAL', 90);
-
-/*12*/
-INSERT INTO
-    events(device_type, sensor_type, pos, operator, operand)
-VALUES
-    (0, 21, 0, 'LESS_EQUAL', 150);
-
-/*events for places visiting detection*/
-/*13*/
-INSERT INTO
-    events(
-        device_type,
-        sensor_type,
-        pos,
-        operator,
-        operand,
-        tag
-    )
-VALUES
-    (0, 2411, 3, 'GREATER_EQUAL', 200, "HOME");
-
-/*14*/
-INSERT INTO
-    events(
-        device_type,
-        sensor_type,
-        pos,
-        operator,
-        operand,
-        tag
-    )
-VALUES
-    (0, 2411, 3, 'LESS_EQUAL', 200, "FRIENDS");
-
-/*15*/
-INSERT INTO
-    events(device_type, sensor_type, pos, operator, operand)
-VALUES
-    (0, 10, 4, 'LESS', -1);
-
-/*events for displacement*/
-/*16*/
-INSERT INTO
-    events(device_type, sensor_type, pos, operator, operand)
-VALUES
-    (0, 10, 4, 'GREATER', 2);
-
-/*17*/
-INSERT INTO
-    events(device_type, sensor_type, pos, operator, operand)
-VALUES
-    (0, 10, 4, 'LESS', -2);
-
-/*events to detect a phone call*/
-INSERT INTO
-    events_for_adls (id_adl, id_event)
-VALUES
-    (1, 1);
-
-INSERT INTO
-    events_for_adls (id_adl, id_event)
-VALUES
-    (1, 2);
-
-INSERT INTO
-    events_for_adls (id_adl, id_event)
-VALUES
-    (1, 3);
-
-INSERT INTO
-    events_for_adls (id_adl, id_event)
-VALUES
-    (1, 4);
+    (0, 2411, 3, 'GREATER', 50, "HOME");
 
 /*events to detect a walk in the outside*/
 INSERT INTO
@@ -296,6 +225,28 @@ INSERT INTO
 VALUES
     (2, 9);
 
+INSERT INTO
+    adls(title, description)
+VALUES
+    (
+        "Paseo por casa, en interior",
+        "Se ha detectado al usuario andando dentro de casa."
+    );
+
+/*10*/
+/*close from home*/
+INSERT INTO
+    events(
+        device_type,
+        sensor_type,
+        pos,
+        operator,
+        operand,
+        tag
+    )
+VALUES
+    (0, 2411, 3, 'LESS_EQUAL', 50, "HOME");
+
 /*events to detect a walk inside house*/
 INSERT INTO
     events_for_adls (id_adl, id_event)
@@ -322,11 +273,32 @@ INSERT INTO
 VALUES
     (3, 10);
 
+INSERT INTO
+    adls(title, description)
+VALUES
+    (
+        "Actividad física avanzada",
+        "Se ha detectado una actividad física detectada, el ritmo cardíaco subió durante la actividad y hubo bastante movimiento."
+    );
+
+/*events to detect a walk*/
+/*11*/
+INSERT INTO
+    events(device_type, sensor_type, pos, operator, operand)
+VALUES
+    (0, 10, 4, 'GREATER', 2);
+
+/*12*/
+INSERT INTO
+    events(device_type, sensor_type, pos, operator, operand)
+VALUES
+    (0, 21, 0, 'GREATER_EQUAL', 85);
+
 /*events to detect workout in the outside*/
 INSERT INTO
     events_for_adls (id_adl, id_event)
 VALUES
-    (4, 5);
+    (4, 11);
 
 INSERT INTO
     events_for_adls (id_adl, id_event)
@@ -336,33 +308,110 @@ VALUES
 INSERT INTO
     events_for_adls (id_adl, id_event)
 VALUES
-    (4, 9);
-
-INSERT INTO
-    events_for_adls (id_adl, id_event)
-VALUES
-    (4, 11);
-
-INSERT INTO
-    events_for_adls (id_adl, id_event)
-VALUES
     (4, 12);
+
+INSERT INTO
+    adls(title, description)
+VALUES
+    (
+        "Visita a casa de amigos detectada",
+        "El usuario ha estado cerca de la casa de unos amigos."
+    );
+
+/*events for places visiting detection*/
+/*14*/
+INSERT INTO
+    events(
+        device_type,
+        sensor_type,
+        pos,
+        operator,
+        operand,
+        tag
+    )
+VALUES
+    (0, 2411, 3, 'GREATER_EQUAL', 100, "HOME");
+
+/*15*/
+INSERT INTO
+    events(
+        device_type,
+        sensor_type,
+        pos,
+        operator,
+        operand,
+        tag
+    )
+VALUES
+    (0, 2411, 3, 'LESS', 100, "FRIENDS");
+
+/*events for places visiting detection*/
+/*version 1: go walking*/
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (5, 14, 1);
+
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (5, 5, 1);
 
 /*events for places visiting detection*/
 INSERT INTO
-    events_for_adls (id_adl, id_event)
+    events_for_adls (id_adl, id_event, version)
 VALUES
-    (5, 13);
+    (5, 6, 1);
 
 INSERT INTO
-    events_for_adls (id_adl, id_event)
+    events_for_adls (id_adl, id_event, version)
 VALUES
-    (5, 14);
+    (5, 15, 1);
+
+/*version 2: go in a vehicle*/
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (5, 14, 2);
+
+/*new events here using positivo and negative strong acceleration*/
+INSERT INTO
+    events_for_adls (id_adl, id_event, version)
+VALUES
+    (5, 15, 2);
 
 INSERT INTO
-    events_for_adls (id_adl, id_event)
+    adls(title, description)
 VALUES
-    (5, 6);
+    (
+        "Desplazamiento por cuenta propia usando un medio de transporte",
+        "Aparentemente se ha usado un medio de transporte para desplazarse."
+    );
+
+/*13*/
+INSERT INTO
+    events(device_type, sensor_type, pos, operator, operand)
+VALUES
+    (0, 21, 0, 'LESS_EQUAL', 150);
+
+/*16*/
+INSERT INTO
+    events(device_type, sensor_type, pos, operator, operand)
+VALUES
+    (0, 10, 4, 'LESS', -1);
+
+/*events for displacement*/
+/*17*/
+INSERT INTO
+    events(device_type, sensor_type, pos, operator, operand)
+VALUES
+    (0, 10, 4, 'GREATER', 2);
+
+/*18*/
+INSERT INTO
+    events(device_type, sensor_type, pos, operator, operand)
+VALUES
+    (0, 10, 4, 'LESS', -2);
 
 /*events for displacement detection*/
 INSERT INTO
@@ -377,30 +426,30 @@ VALUES
 
 /*Example: how to add a new adl*/
 /*INSERT INTO
-    adls(title, description)
-VALUES
-    (
-        "Agitado teléfono",
-        "Hemos visto que has agitado el teléfono ferozmente, se  ha detectado pues esta pseudoadl de prueba"
-    );
-
-INSERT INTO
-    events(device_type, sensor_type, pos, operator, operand)
-VALUES
-    (0, 10, 4, 'GREATER', 5);
-
-INSERT INTO
-    events(device_type, sensor_type, pos, operator, operand)
-VALUES
-    (0, 10, 4, 'LESS', -5);
-
-INSERT INTO
-    events_for_adls (id_adl, id_event, version)
-VALUES
-    (7, 17, 1);
-
-INSERT INTO
-    events_for_adls (id_adl, id_event)
-VALUES
-    (7, 18, 1);
-    */
+ adls(title, description)
+ VALUES
+ (
+ "Agitado teléfono",
+ "Hemos visto que has agitado el teléfono ferozmente, se  ha detectado pues esta pseudoadl de prueba"
+ );
+ 
+ INSERT INTO
+ events(device_type, sensor_type, pos, operator, operand)
+ VALUES
+ (0, 10, 4, 'GREATER', 5);
+ 
+ INSERT INTO
+ events(device_type, sensor_type, pos, operator, operand)
+ VALUES
+ (0, 10, 4, 'LESS', -5);
+ 
+ INSERT INTO
+ events_for_adls (id_adl, id_event, version)
+ VALUES
+ (7, 17, 1);
+ 
+ INSERT INTO
+ events_for_adls (id_adl, id_event)
+ VALUES
+ (7, 18, 1);
+ */
